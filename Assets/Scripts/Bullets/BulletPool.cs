8f6b34eaef007d82bfa6baa49aaededbf4b80025
nayan <nayankamalverma@gmail.com>
2024-10-14 22:44:10 +0530

    created bulletpool script

---
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CosmicCuration.Bullets
+{
+	public class BulletPool
+	{
+		private BulletView bulletView;
+		private BulletScriptableObject bulletScriptableObject;
+		private List<PooledBullet> pooledBullets = new List<PooledBullet>();
+
+
+		public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject)
+		{
+			this.bulletView = bulletView;
+			this.bulletScriptableObject = bulletScriptableObject;
+		}
+
+		public class PooledBullet
+		{
+			public BulletController Bullet;
+			public bool isUsed;
+		}
+	}
+}
