e7e214fe6d83e869e6ec3f0a925b8cc9175319ec
nayan <nayankamalverma@gmail.com>
2024-10-17 21:15:14 +0530

    Createed Powerup Pool

---
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -0,0 +1,25 @@
+﻿using CosmicCuration.Utilities;
+using System;
+
+namespace CosmicCuration.PowerUps
+{
+    public class PowerUpPool : GenericObjectPool<PowerUpController>
+    {
+        private PowerUpData powerUpData;
+
+        public PowerUpController GetPowerUp<T>(PowerUpData powerUpData) where T : PowerUpController
+        {
+            this.powerUpData = powerUpData;
+            return GetItem<T>();
+        }
+
+        protected override PowerUpController CreateItem<T>()
+        {
+            
+            if(typeof(T)==typeof(Shield))return new Shield(powerUpData);
+            else if(typeof(T)==typeof(RapidFire)) return new RapidFire(powerUpData);
+            else if(typeof(T)==typeof(DoubleTurret))  return new DoubleTurret(powerUpData);
+            else throw new NotSupportedException("Power up type not supported.");
+        }
+    }
+}
